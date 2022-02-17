3775bb712d317f39f864e7b377a88f836e3bae10
NoF0rte <nof0rte@users.noreply.github.com>
2022-02-16 19:05:04 -0700

    Created the CDP struct that has the parsing functions. Went the viper route and have global functions that use the global CDP instance but one can create their own

--- cdp.go
+++ cdp.go
@@ -2,20 +2,62 @@ package gocdp
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"os"
 )
 
-var DefaultParsers = []Parser{
+var c *CDP
+var errNoParser = errors.New("no parser found")
+
+var defaultParsers = []Parser{
 	FfufParser{},
 	GobusterParser{},
 }
 
-func SmartParseFiles(files []string, parsers ...Parser) (CDResults, error) {
+type Option func(*CDP)
+
+// FailNoParserErrs will enable failing when no parser is found when parsing multiple files
+func FailNoParserErrs() Option {
+	return func(c *CDP) {
+		c.failNoParserErr = true
+	}
+}
+
+// DefaultParsers sets the default parsers used when no parser is specified
+func DefaultParsers(parsers ...Parser) Option {
+	return func(c *CDP) {
+		c.defaultParsers = parsers
+	}
+}
+
+type CDP struct {
+	defaultParsers  []Parser
+	failNoParserErr bool
+}
+
+func New(options ...Option) *CDP {
+	cdp := &CDP{}
+	for _, option := range options {
+		option(cdp)
+	}
+	if len(cdp.defaultParsers) == 0 {
+		cdp.defaultParsers = defaultParsers
+	}
+	return cdp
+}
+
+func (cdp *CDP) SmartParseFiles(files []string, parsers ...Parser) (CDResults, error) {
 	var allResults CDResults
 	for _, file := range files {
-		results, err := SmartParseFile(file)
+		results, err := cdp.SmartParseFile(file)
 		if err != nil {
+			if err == errNoParser {
+				if cdp.failNoParserErr {
+					return nil, fmt.Errorf("no parser found for file '%s'", file)
+				}
+				continue
+			}
 			return nil, err
 		}
 
@@ -24,24 +66,24 @@ func SmartParseFiles(files []string, parsers ...Parser) (CDResults, error) {
 	return allResults, nil
 }
 
-func SmartParseFile(file string, parsers ...Parser) (CDResults, error) {
+func (cdp *CDP) SmartParseFile(file string, parsers ...Parser) (CDResults, error) {
 	f, err := os.Open(file)
 	if err != nil {
 		return nil, err
 	}
 	defer f.Close()
 
-	return SmartParse(f, parsers...)
+	return cdp.SmartParse(f, parsers...)
 }
 
-func SmartParse(reader io.Reader, parsers ...Parser) (CDResults, error) {
+func (cdp *CDP) SmartParse(reader io.Reader, parsers ...Parser) (CDResults, error) {
 	bytes, err := io.ReadAll(reader)
 	if err != nil {
 		return nil, err
 	}
 
 	if len(parsers) == 0 {
-		parsers = DefaultParsers
+		parsers = cdp.defaultParsers
 	}
 
 	input := string(bytes)
@@ -54,7 +96,7 @@ func SmartParse(reader io.Reader, parsers ...Parser) (CDResults, error) {
 	}
 
 	if parser == nil {
-		return nil, errors.New("no parser found for file")
+		return nil, errNoParser
 	}
 
 	output, err := parser.Parse(input)
@@ -62,4 +104,20 @@ func SmartParse(reader io.Reader, parsers ...Parser) (CDResults, error) {
 		return nil, err
 	}
 	return output, nil
+}
+
+func SmartParseFiles(files []string, parsers ...Parser) (CDResults, error) {
+	return c.SmartParseFiles(files, parsers...)
+}
+
+func SmartParseFile(file string, parsers ...Parser) (CDResults, error) {
+	return c.SmartParseFile(file, parsers...)
+}
+
+func SmartParse(reader io.Reader, parsers ...Parser) (CDResults, error) {
+	return c.SmartParse(reader, parsers...)
+}
+
+func init() {
+	c = New()
 }
